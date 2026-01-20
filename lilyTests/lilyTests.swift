651373c27c709cfd59810bd5129e3ed80792e0c3
Cameron <cpjules@gmail.com>
2026-01-20 18:36:04 +0000

    Initial Commit

---
+++ lilyTests/lilyTests.swift
@@ -0,0 +1,17 @@
+//
+//  lilyTests.swift
+//  lilyTests
+//
+//  Created by Cameron Jules on 20/01/2026.
+//
+
+import Testing
+@testable import lily
+
+struct lilyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
